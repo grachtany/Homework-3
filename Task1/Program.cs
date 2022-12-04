3ae12d03892fdb145157830e54f4777bc868946b
tatangracheva <grachtany@yandex.ru>
2022-12-04 13:52:44 +0300

    Выполнено задание 23

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear ();
+
+int SetString (string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+double Qub (int a)
+{   
+    double b = Math.Pow (a,3);
+    // int b = a*a*a;
+    return (b);
+
+}
+int x = SetString ("Введите число N = ");
+int count = 1;
+while (count <= x)
+{
+    double a = Qub(count);
+    Console.Write ($" {a},");
+    count++;
+}
